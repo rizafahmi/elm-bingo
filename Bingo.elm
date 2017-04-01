45bf64bfd83c3fb1994c5f0a690a945c767ab342
Riza Fahmi <rizafahmi@gmail.com>
2017-04-01 18:32:52 +0700

    Finsih Rendering HTML

--- Bingo.elm
+++ Bingo.elm
@@ -1,7 +1,40 @@
 module Bingo exposing (..)
 
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+playerInfo name gameNumber =
+    name ++ " - Game #" ++ (toString gameNumber)
+
+
+viewPlayer name gameNumber =
+    let
+        playerInfoText =
+            playerInfo name gameNumber
+                |> String.toUpper
+                |> Html.text
+    in
+        h2 [ id "info", class "classy" ] [ playerInfoText ]
+
+
+viewHeader title =
+    header []
+        [ h1 [] [ text title ] ]
+
+
+viewFooter =
+    footer []
+        [ a [ href "http://elm-lang.org/" ] [ text "Powered by Elm" ] ]
+
+
+view =
+    div [ class "content" ]
+        [ viewHeader "Buzzword Bingo"
+        , viewPlayer "Riza" 22
+        , viewFooter
+        ]
 
 
 main =
-    Html.text "Hello, Elm"
+    view
